6bdef74c529d994449faba3ed03349b2d8ecce76
Ahmed Maher <ahmedsayed_g@sci.asu.edu.eg>
2026-03-03 21:39:55 +0200

    Add project files.

---
+++ Session 11/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Session_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Singelton Design Pattern
+            //Car car = new Car()
+            //{
+            //    Id = 1,
+            //    Model = "BMW"
+            //}; 
+
+            Car car1 = Car.GetCar();
+
+            Console.WriteLine(car1.GetHashCode());
+            Car car2 = Car.GetCar();
+
+            Console.WriteLine(car2.GetHashCode());
+            Car car3 = Car.GetCar();
+
+            Console.WriteLine(car3.GetHashCode());
+            #endregion
+        }
+    }
+}
